887a07308ed16c45dde10ad9e4810301bc6cceb9
massimilianoregis <massimiliano.regis@gmail.com>
2025-07-20 20:25:57 +0200

    android compatible

--- timing.js
+++ timing.js
@@ -1,21 +1,44 @@
-const schedule = require('node-schedule');
 
 var Lights= require("./Lights")
 var light = new Lights();
 light.ledSoggiorno(false);
 light.ledNotte(false);
 
-schedule.scheduleJob({hour: 20, minute: 15}, function(){
-    light.ledSoggiorno(true);
+
+function scheduleNextRun(hour, minute, callback) {
+  const now = new Date();
+  const next = new Date(now);
+
+  next.setDate(now.getDate() + 1); // giorno dopo
+  next.setHours(hour, minute, 0, 0); // ora e minuto obiettivo
+
+  const delay = next.getTime() - now.getTime();
+
+  console.log(`Schedulo il job per: ${next}`);
+  console.log(`Delay in ms: ${delay}`);
+
+  setTimeout(() => {
+    callback();
+
+    // Se vuoi ri-schedulare automaticamente per il giorno dopo:
+    scheduleNextRun(hour, minute, callback);
+  }, delay);
+}
+
+  scheduleNextRun(20, 30, function(){
+      light.ledSoggiorno(true);
   });
-schedule.scheduleJob({hour: 22, minute: 0}, function(){
+
+
+  scheduleNextRun(22, 0,  function(){
     light.ledNotte(true);
   });
 
 
-schedule.scheduleJob({hour: 1, minute: 0}, function(){
-  light.ledSoggiorno(false);
-  light.ledNotte(false);
-});
+  scheduleNextRun(1, 0, function(){
+    light.ledSoggiorno(false);
+    light.ledNotte(false);
+  });
+
 
 console.log("waiting")
