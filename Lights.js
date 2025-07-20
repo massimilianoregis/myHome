52f172f781997be298a8d151d6356d22359c034a
massimilianoregis <massimiliano.regis@gmail.com>
2025-07-20 20:28:14 +0200

    axios is not compatible with android

--- Lights.js
+++ Lights.js
@@ -1,14 +1,34 @@
 const lights=require("./const");
-var axios = require("axios").default
+const https = require('https');
 const url="http://192.168.0.2/forms.htm";
+
+function httpGet(url, callback) {
+    https.get(url, (res) => {
+      let data = '';
+  
+      // Ricevo i dati a pezzi
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+  
+      // Fine risposta
+      res.on('end', () => {
+        callback(null, data);
+      });
+  
+    }).on('error', (err) => {
+      callback(err);
+    });
+  }
+
 class Lights{
     ledSoggiorno(value){
         console.log(`${url}?led${lights.LED_SOGGIORNO}=${value?'1':'0'}`)
-        axios.get(`${url}?led${lights.LED_SOGGIORNO}=${value?'1':'0'}`)
+        httpGet(`${url}?led${lights.LED_SOGGIORNO}=${value?'1':'0'}`)
     }
     ledNotte(value){
         console.log(`${url}?led${lights.LED_NOTTE}=${value?'1':'0'}`)
-        axios.get(`${url}?led${lights.LED_NOTTE}=${value?'1':'0'}`)
+        httpGet(`${url}?led${lights.LED_NOTTE}=${value?'1':'0'}`)
     }
 }
 
